736d68df55d4b3df60bf404043077f5bc3792843
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-02-10 20:07:07 -0300

    Ajuste header

--- resources/views/hospedes/index.blade.php
+++ resources/views/hospedes/index.blade.php
@@ -1,17 +1,7 @@
 <!doctype html>
 <html lang="en">
+@include('layout.header')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <title>Employee Record</title>
-</head>
 
 <body>
 
