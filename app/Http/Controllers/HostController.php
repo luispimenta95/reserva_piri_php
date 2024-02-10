27fcf3ea7b43e0ac41181f93f431891a9f029bd7
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2024-02-10 16:17:17 -0300

    Reserva salva

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -25,21 +25,35 @@ class HostController extends Controller
 
 
         $hospedes = array();
+        $idsHospedes = array();
+        $informacoesReserva = [];
 
         for ($i = 0; $i < count($nome); $i++) {
-            $informacoes = [
+            $informacoesHospedes = [
                 'nome' => $nome[$i],
                 'cpf' => $cpf[$i],
                 'nascimento' => $nascimento[$i],
                 'email' => $email[$i],
                 'telefone' => $telefone[$i]
             ];
-            Database::table('hospedes')->insert($informacoes);
-            array_push($hospedes, $informacoes);
+            $lastId = Database::table('hospedes')->insertGetId($informacoesHospedes);
+            array_push($idsHospedes, $lastId);
+            array_push($hospedes, $informacoesHospedes);
         }
+        //Iinformacoes Hospedes
         $data['dataInicial'] = $request->dataInicial;
         $data['dataFinal'] = $request->dataFinal;
         $data['hospedes'] = $hospedes;
+        // Fim das informacoes hospedes
+
+        //Reserva
+        $informacoesReserva['dataInicial'] = $request->dataInicial;
+        $informacoesReserva['dataFinal'] = $request->dataFinal;
+        $informacoesReserva['hospedes'] = json_encode($idsHospedes);
+
+        Database::table('reserva')->insert($informacoesReserva);
+
+        //Fim reserva
         return $app->gerarPdf($data);
     }
 }
